2a2306b1d3eac9fff5aecb591e411861681f25a1
Milica <pavlovicm995@yahoo.com>
2022-06-30 12:14:36 +0200

    Card animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import {motion} from 'framer-motion';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.div transition={{ layout: {duration:1, type: "spring"} }} 
+      Layout 
+      onClick={() => setIsOpen(!isOpen)} 
+      className="card"
+      style={{   borderRadius: "1rem", boxShadov: '0px 10px 30px rgba(0,0,0,0.5)' }}>
+        <motion.h2 Layout="position">Quote 🚀</motion.h2>
+        {isOpen && ( 
+        <motion.div initial={{opacity: 0}} animate={{ opacity: 1}} transition={{ duration: 1 }} 
+        className="expand" exit={{ opacity: 0 }}>
+          <p>
+          Master the basics. Then practice them every day without fail. 
+          Small disciplines repeated with consistency every day lead to great achievements gained slowly over time.
+          </p>
+          <button>John C. Maxwell</button>
+        </motion.div>
+        )}
+      </motion.div>
     </div>
   );
 }
